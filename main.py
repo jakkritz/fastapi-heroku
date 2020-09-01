843d3686be5df256f79efa4c9bc9d9d92765c63a
jakkrit <jakkrit>
2020-09-01 22:28:00 +0700

    heroku link

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, APIRouter
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "https://fastapi-prototyping.herokuapp.com/docs"}
+
+
+@app.post('/dialogflow/fulfillment/payment')
+def dialogflow_scb_payment(request):
+    return {k: v for k, v in request}
